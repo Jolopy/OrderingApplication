2b138ec87280f7d0f624ac48a289477da1d87259
Jolopy <cczechowicz@gmail.com>
2018-02-01 14:19:38 -0500

    Added a counter to the code

--- RUAutomation/app/src/main/java/com/example/cczec/ruautomation/MainActivity.java
+++ RUAutomation/app/src/main/java/com/example/cczec/ruautomation/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,7 +12,12 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button1;
+    private Button button1,button2;
+    private TextView textView;
+
+    public int counter = 0;
+    public String total = "total";
+
 
 
     @Override
@@ -24,20 +30,30 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("OrderNumber");
 
         button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        textView = (TextView)findViewById(R.id.textView);
+
 
         button1.setOnClickListener(new View.OnClickListener() {
             FirebaseDatabase database = FirebaseDatabase.getInstance();
             DatabaseReference ryRef = database.getReference("OrderNumber");
             @Override
             public void onClick(View view) {
-                for(int i = 0; i < 5; i++){
+                for(int i = 0; i < counter; i++){
                     ryRef.child("number").child(Integer.toString(i)).setValue("Hello, World! ");
                 }
-
             }
         });
 
-
+        button2.setOnClickListener(new View.OnClickListener() {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference rxRef = database.getReference("OrderNumber");
+            @Override
+            public void onClick(View view) {
+               counter = counter + 1;
+                textView.setText("Total: "+counter+"");
+            }
+        });
 
     }
 }
