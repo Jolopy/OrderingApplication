54f2e4db49d4f8d7c0e43edf3a2c297e1e83cf40
elyfialkoff <mouseen92@gmail.com>
2018-02-02 11:16:42 -0500

    InventorySystem

---
+++ RUAutomation/app/src/main/java/com/example/cczec/ruautomation/InventoryActivity.java
@@ -0,0 +1,9 @@
+package com.example.cczec.ruautomation;
+
+/**
+ * Created by Ely on 2/2/2018.
+ * This activity is going to be used by Joe, Dax, and Manthan to develop the Inventory system 
+ */
+
+public class InventoryActivity {
+}
