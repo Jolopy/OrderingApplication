24ab7ba90dbb8f7d208fe0d90fb4baa50ddc40e6
elyfialkoff <mouseen92@gmail.com>
2018-03-24 16:52:00 -0400

    changes

--- app/src/main/java/com/example/cczec/templateruapplication/MainActivity.java
+++ app/src/main/java/com/example/cczec/templateruapplication/MainActivity.java
@@ -10,12 +10,14 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView orderReadyFB;
     private DatabaseReference mDatabaseFB;
+    private Button confirmBtnFB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        confirmBtnFB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mDatabaseFB.child("")
+            }
+        });
+
 
 
         //("https://ruautomation-918ed.firebaseio.com/TableNumber/1");
